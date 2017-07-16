0d860bba1043ad858370ee62a89d106c73d5fea5
z-teacher <1536164129@qq.com>
2017-07-16 20:32:00 +0800

    adjust

--- node/routers/shop.js
+++ node/routers/shop.js
@@ -4,42 +4,42 @@ function Shop(){
 	var json= {
           roundList:[
           {
-          	imageUrl:prefix+'img/tips/tb1.png',
+          	imageUrl:prefix+'img/shop/tb1.png',
           	roundBg:'#ffb20c;',
           	des:'中西药'
           },
           {
-          	imageUrl:prefix+'img/tips/tb2.png',
+          	imageUrl:prefix+'img/shop/tb2.png',
           	roundBg:'#ff4400;',
           	des:'儿童用药'
           },
           {
-          	imageUrl:prefix+'img/tips/tb3.png',
+          	imageUrl:prefix+'img/shop/tb3.png',
           	roundBg:'#22a6f9;',
           	des:'医疗用药'
           },
           {
-          	imageUrl:prefix+'img/tips/tb4.png',
+          	imageUrl:prefix+'img/shop/tb4.png',
           	roundBg:'#e45734;',
           	des:'医疗器械'
           },
           {
-          	imageUrl:prefix+'img/tips/tb5.png',
+          	imageUrl:prefix+'img/shop/tb5.png',
           	roundBg:'#e2000c;',
           	des:'养生保健'
           },
           {
-          	imageUrl:prefix+'img/tips/tb6.png',
+          	imageUrl:prefix+'img/shop/tb6.png',
           	roundBg:'#ff7e00;',
           	des:'隐形眼镜'
           },
           {
-          	imageUrl:prefix+'img/tips/tb7.png',
+          	imageUrl:prefix+'img/shop/tb7.png',
           	roundBg:'#ff4d49;',
           	des:'美容护肤'
           },
           {
-          	imageUrl:prefix+'img/tips/tb8.png',
+          	imageUrl:prefix+'img/shop/tb8.png',
           	roundBg:'#eb3c78;',
           	des:'成人用品'
           }
@@ -50,17 +50,17 @@ function Shop(){
             {
           	productName:'【力魂】灵芝益寿胶囊',
           	price:'8.99',
-          	productImage:prefix+'img/goods/s.png'
+          	productImage:prefix+'img/shop/s.png'
             },
             {
           	productName:'【永安康健】玛咖',
           	price:'119.00',
-          	productImage:prefix+'img/goods/s1.png'
+          	productImage:prefix+'img/shop/s1.png'
             },        
             {
           	productName:'【金奥力】蜂胶胶囊',
           	price:'138.00',
-          	productImage:prefix+'img/goods/s2.png'
+          	productImage:prefix+'img/shop/s2.png'
             }      
             ]
           },
@@ -70,17 +70,17 @@ function Shop(){
             {
           	productName:'鸿茅药酒（500ml）',
           	price:'196.00',
-          	productImage:prefix+'img/goods/s5.png'
+          	productImage:prefix+'img/shop/s5.png'
             },
             {
           	productName:'【恒诚】跌打榜药酒',
           	price:'13.80',
-          	productImage:prefix+'img/goods/s3.png'
+          	productImage:prefix+'img/shop/s3.png'
             },        
             {
           	productName:'【万通】颈痛灵药酒',
           	price:'25.00',
-          	productImage:prefix+'img/goods/s4.png'
+          	productImage:prefix+'img/shop/s4.png'
             }      
             ]
           },
@@ -90,17 +90,17 @@ function Shop(){
             {
           	productName:'【姿美堂】胶原蛋白粉',
           	price:'8.99',
-          	productImage:prefix+'img/goods/s6.png'
+          	productImage:prefix+'img/shop/s6.png'
             },
             {
           	productName:'新加坡DRC亮白蚕丝精华液',
           	price:'15.80',
-          	productImage:prefix+'img/goods/s7.png'
+          	productImage:prefix+'img/shop/s7.png'
             },        
             {
           	productName:'【爱美得】大眼睛睫毛膏',
           	price:'36.00',
-          	productImage:prefix+'img/goods/s8.png'
+          	productImage:prefix+'img/shop/s8.png'
             }      
             ]
           } 
