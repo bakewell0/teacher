e16651e13074c42391898fb25aae0d1d00206498
Johnber <1536164129@qq.com>
2017-09-01 18:04:10 +0800

    sql

--- sql/orders.sql
+++ sql/orders.sql
@@ -1,21 +1,19 @@
 /*
- Navicat MySQL Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : tiaozao
- Source Server Type    : MySQL
- Source Server Version : 50719
- Source Host           : localhost:3306
- Source Schema         : tiaozao
+Source Server         : localhost_3306
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : tiaozao
 
- Target Server Type    : MySQL
- Target Server Version : 50719
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
 
- Date: 16/08/2017 10:27:09
+Date: 2017-09-01 12:50:31
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for orders
@@ -32,7 +30,14 @@ CREATE TABLE `orders` (
   `message` varchar(1000) DEFAULT NULL,
   `totalNum` varchar(20) DEFAULT NULL,
   `productId` varchar(1000) DEFAULT NULL,
+  `productNum` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=168 DEFAULT CHARSET=utf8;
 
-SET FOREIGN_KEY_CHECKS = 1;
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
+INSERT INTO `orders` VALUES ('162', '2017-08-31 08:41:12', '2017-08-31 08:41:12', '4', '包邮', '￥66000', '0', '', '30', '[1,2,3]', '[10,3,2]');
+INSERT INTO `orders` VALUES ('164', '2017-08-31 11:42:06', '2017-08-31 11:42:06', '4', '包邮', '￥66000', '0', '', '30', '[1,2,3]', '[10,3,2]');
+INSERT INTO `orders` VALUES ('166', '2017-09-01 02:22:00', '2017-09-01 02:22:00', '6', '快递:￥5.00', '17500', '1', '', '1', '[2]', '[5]');
+INSERT INTO `orders` VALUES ('167', '2017-09-01 03:04:12', '2017-09-01 03:04:12', '6', '快递:￥5.00', '15400', '0', '', '1', '[1]', '[7]');
--- sql/products.sql
+++ sql/products.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-08-07 21:17:35
+Date: 2017-09-01 12:50:42
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -38,7 +38,7 @@ CREATE TABLE `products` (
 -- ----------------------------
 -- Records of products
 -- ----------------------------
-INSERT INTO `products` VALUES ('1', 'http://192.168.0.109:8020/node/img/productList/phone.jpg', '二手apple/苹果iphone5s手机三网无锁移动4G联通电信', '包邮', '上海', '挂起', '2200', '4868', '0', '二手apple/苹果iphone5s手机三网无锁移动4G联通电信', '2017-08-04 01:37:12', '2017-08-04 01:37:12');
-INSERT INTO `products` VALUES ('2', 'http://192.168.0.109:8020/node/img/productList/laptop.jpg', '二手apple/苹果MacBookAirMC965CH/A笔记本电脑', '包邮', '上海', '挂起', '3500', '5288', '0', '二手apple/苹果MacBookAirMC965CH/A笔记本电脑', '2017-08-04 01:37:12', '2017-08-04 01:37:12');
-INSERT INTO `products` VALUES ('3', 'http://192.168.0.109:8020/node/img/productList/book.jpg', '高等数学(上册)大学高等数学课程创新新教材丘维声', '包邮', '上海', '挂起', '35', '128', '1', '高等数学(上册)大学高等数学课程创新新教材丘维声', '2017-08-04 01:37:12', '2017-08-04 01:37:12');
-INSERT INTO `products` VALUES ('4', 'http://192.168.0.109:8020/node/img/productList/bicycle.jpg', '捷安特24速变一体轮可折叠自行车', '包邮', '上海', '挂起', '350', '1358', '0', '捷安特24速变一体轮可折叠自行车', '2017-08-04 01:37:12', '2017-08-04 01:37:12');
+INSERT INTO `products` VALUES ('1', 'http://192.168.0.135:8020/node/img/productList/phone.jpg', '二手apple/苹果iphone5s手机三网无锁移动4G联通电信', '包邮', '上海', '挂起', '2200', '4868', '0', '二手apple/苹果iphone5s手机三网无锁移动4G联通电信', '2017-08-04 01:37:12', '2017-08-04 01:37:12');
+INSERT INTO `products` VALUES ('2', 'http://192.168.0.135:8020/node/img/productList/laptop.jpg', '二手apple/苹果MacBookAirMC965CH/A笔记本电脑', '包邮', '上海', '挂起', '3500', '5288', '0', '二手apple/苹果MacBookAirMC965CH/A笔记本电脑', '2017-08-04 01:37:12', '2017-08-04 01:37:12');
+INSERT INTO `products` VALUES ('3', 'http://192.168.0.135:8020/node/img/productList/book.jpg', '高等数学(上册)大学高等数学课程创新新教材丘维声', '包邮', '上海', '挂起', '35', '128', '1', '高等数学(上册)大学高等数学课程创新新教材丘维声', '2017-08-04 01:37:12', '2017-08-04 01:37:12');
+INSERT INTO `products` VALUES ('4', 'http://192.168.0.135:8020/node/img/productList/bicycle.jpg', '捷安特24速变一体轮可折叠自行车', '包邮', '上海', '挂起', '350', '1358', '0', '捷安特24速变一体轮可折叠自行车', '2017-08-04 01:37:12', '2017-08-04 01:37:12');
