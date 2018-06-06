d19dcd48610852a2aeb82d9c0de50d566b2db4f5
unknown <1536164129@qq.com>
2018-06-06 18:17:03 +0800

    g

---
+++ wg/gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp'),  
+    runSequence = require('run-sequence'),  
+    rev = require('gulp-rev'),  
+    revCollector = require('gulp-rev-collector');  
+  
+//定义css、js源文件路径  
+var cssSrc = 'css/*.css',  
+    jsSrc = 'js/*.js';  
+
+//CSS生成文件hash编码并生成 rev-manifest.json文件名对照映射  
+gulp.task('revCss', function(){  
+    return gulp.src(cssSrc)  
+        .pipe(rev())  
+        .pipe(gulp.dest('./dist/css'))  
+        .pipe(rev.manifest())  
+        .pipe(gulp.dest('./dist/css'));  
+});  
+  
+  
+//js生成文件hash编码并生成 rev-manifest.json文件名对照映射  
+gulp.task('revJs', function(){  
+    return gulp.src(jsSrc)  
+        .pipe(rev())                                //给文件添加hash编码  
+        .pipe(gulp.dest('./dist/js'))  
+        .pipe(rev.manifest())                       //生成rev-mainfest.json文件作为记录  
+        .pipe(gulp.dest('./dist/js'));  
+});  
+  
+  
+//Html替换css、js文件版本  
+gulp.task('revHtmlCss', function () {  
+    return gulp.src(['./dist/css/*.json', 'index.html'])  
+        .pipe(revCollector())                         //替换html中对应的记录  
+        .pipe(gulp.dest('./dist'));                     //输出到该文件夹中  
+});  
+
+gulp.task('revHtmlJs', function () {  
+    return gulp.src(['./dist/js/*.json', './dist/index.html'])  
+        .pipe(revCollector())  
+        .pipe(gulp.dest('./dist'));  
+});  
+
+//开发构建  
+gulp.task('dev', function (done) {  
+    condition = false;  
+    //依次顺序执行  
+    runSequence(  
+        ['revCss'],  
+        ['revHtmlCss'],  
+        ['revJs'],  
+        ['revHtmlJs'],  
+        done);  
+});
