3f4827d95299007ccb5bdddceeb4f1aafd76e26b
unknown <1536164129@qq.com>
2018-07-10 13:30:20 +0800

    breadcrumb

---
+++ invent/src/components/breadcrumb.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="breadcrumbs">
+		{{path1}} > {{path2}}
+	</div>
+</template>
+
+<script>
+	export default {
+		props:['path1','path2']
+	}
+</script>
+
+<style>
+	.breadcrumbs{
+		background: #f5f5f5;
+		height: 40px;
+		line-height: 40px;
+		margin: 0 -20px;
+		padding-left: 20px;
+		color:#585858;
+	}
+</style>
